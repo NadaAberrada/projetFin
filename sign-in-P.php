bbd7a80beb7065178b859d926a28efa6f29f806c
Nada Aberrada <aberradanada@gmail.com>
2023-04-14 12:59:10 +0000

    Sign in + sign up

---
+++ sign-in-P.php
@@ -0,0 +1,84 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sign In Form</title>
+        <!-- Add Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <!-- Add Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <!-- Custom CSS -->
+        <style>
+            html, body {
+                height: 100%;
+                background-color: #f8f9fa;
+            }
+            body {
+                height: 100%;
+                background-image: url('./img/doctorHeader.jpg');
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+            }
+            .overlay {
+                background-color: rgba(255, 255, 255, 0.7);
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+            }
+
+
+
+            .form-container {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 100%;
+            }
+
+            .form-wrapper {
+                background-color: #ffffff;
+                padding: 30px;
+                border-radius: 5px;
+                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+                width: 100%;
+                max-width: 400px;
+            }
+
+            .logo {
+                font-size: 36px;
+                font-weight: bold;
+                margin-bottom: 20px;
+                color: #4a4a4a;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="container form-container">
+            <div class="form-wrapper">
+                <h2 class="logo">YourBrand</h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" required>
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="rememberMe">
+                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-3 w-100">Sign In</button>
+                    <p class="text-center">Don't have an account? <a href="#">Sign up</a></p>
+                </form>
+            </div>
+        </div>
+    </body>
+    </html>
